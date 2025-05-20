a20915de67c219c94a68114b1b1b0aa673eeb0cc
joavincus <104866052+joavincus@users.noreply.github.com>
2025-05-20 00:03:33 -0300

    Update disciplina.ts

--- disciplina.ts
+++ disciplina.ts
@@ -1,17 +1,45 @@
 export class Disciplina {
-    private codigo: number
+    private cod: number
+    private nome: string
+    private ch: number
+    private docente: string
 
-    constructor(cod: number) {
-        this.codigo = cod
+    constructor(cod: number, nome: string, ch: number, docente: string) {
+        this.cod = cod
+        this.nome = nome
+        this.ch = ch
+        this.docente = docente
     }
 
-    obterCodigo(): number {
-        return this.codigo
+    obterCod(): number {
+        return this.cod
     }
 
-    inserirCodigo(cod: number): void {
-        this.codigo = cod
+    inserirCod(cod: number): void {
+        this.cod = cod
+    }
 
-        // Disciplina
+    obterNome(): string {
+        return this.nome
+    }
+
+    inserirNome(nome: string): void {
+        this.nome = nome
+    }
+
+    obterCH(): number {
+        return this.ch
+    }
+
+    inserirCH(ch: number): void {
+        this.ch = ch
+    }
+
+    obterDocente(): string {
+        return this.docente
+    }
+
+    inserirDocente(docente: string): void {
+        this.docente = docente
     }
 }
