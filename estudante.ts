85db9108a7fc1ee27e60d44d17ba29b4e2ee7af4
joavincus <104866052+joavincus@users.noreply.github.com>
2025-05-20 00:03:16 -0300

    Update estudante.ts

--- estudante.ts
+++ estudante.ts
@@ -4,28 +4,41 @@ export enum Curso {
     REDES = "Redes"
 }
 
-export class Estudante{
+export class Estudante {
     private matricula: string
     private nome: string
     private curso: Curso
     private periodo: number
 
-    constructor(mat: string, nome: string, 
-        cr: Curso = Curso.INFO,
-        pr:number = 1){
+    constructor(mat: string, nome: string, cr: Curso = Curso.INFO, pr: number = 1) {
         this.matricula = mat
         this.nome = nome
         this.curso = cr
         this.periodo = pr
     }
 
-    alterarPeriodo(per: number){
-        if (per >= 1 && per <= 9){
-            this.periodo = per
-        }
+
+    obterMatricula(): string {
+        return this.matricula
+    }
+
+
+    obterNome(): string {
+        return this.nome
+    }
+
+
+    obterCurso(): string {
+        return this.curso
     }
 
     obterPeriodo(): number {
         return this.periodo
+    }
+    
+    alterarPeriodo(per: number): void {
+        if (per >= 1 && per <= 9) {
+            this.periodo = per
+        }
     }
 }
